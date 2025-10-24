3affd89ff4ac86578cb30ae7ecc493768211dacc
mkaufman2023 <mkaufman2023@gmail.com>
2025-10-24 13:25:11 -0500

    versioning change

--- barchart/__init__.py
+++ barchart/__init__.py
@@ -5,8 +5,10 @@
 """
 from . import etfs, options, stocks
 
-try:
-    from importlib.metadata import version
-    __version__ = version("barchart")
-except Exception:
-    __version__ = "0.0.0"
+__version__ = "0.0.2"
+
+# try:
+#     from importlib.metadata import version
+#     __version__ = version("barchart")
+# except Exception:
+#     __version__ = "0.0.0"
