a53dde2ea4e5fd42548056cb32db0829d77d5cc6
mkaufman2023 <mkaufman2023@gmail.com>
2025-10-27 12:15:15 -0500

    added get_unusual_options_activity function

--- barchart/options.py
+++ barchart/options.py
@@ -3,16 +3,178 @@
 
 
 """
+import datetime
+from typing import Callable
 
-def test():
+
+def get_unusual_options_activity(
+        filters: dict[str, Callable] = None,
+        groupby_symbol: bool = False,
+        groupby_optiontype: bool = False,
+        groupby_expiration: bool = False,
+    ) -> list[dict[str, str|float|int|datetime.date|datetime.datetime]]:
     """
-    ## barchart.options.test
+    Fetches unusual options activity data from Barchart.
+
+    ### Parameters
+    - `filters` ( *dict* ) - Optional filters to apply to the data. Possible keys include:
+        - `symbol` - Filter by specific stock symbol(s).
+        - `expiration_date` - Filter by expiration date.
+        - `days_to_expiration` - Filter by days to expiration.
+        - `option_type` - Filter by option type (e.g., "call", "put").
+        - `volume` - Filter by volume.
+        - `open_interest` - Filter by open interest.
+        - `volume_open_interest_ratio` - Filter by volume/open interest ratio.
+    - `groupby_symbol` ( *bool* ) - If `True`, groups the results by symbol instead of returning all entries as a flat list.
+    - `groupby_optiontype` ( *bool* ) - If `True`, groups the results by option type instead of returning all entries as a flat list.
+    - `groupby_expiration` ( *bool* ) - If `True`, groups the results by expiration date instead of returning all entries as a flat list.
+
+    ### Returns
+    - `list[dict]` - A list of dictionaries containing unusual options activity data.
+
+    ### Formatting Examples for `filters`
+    - `symbol`
+        - `"symbol": lambda x: x["symbol"] == "RXRX"`           - Return only entries with symbol RXRX.
+        - `"symbol": lambda x: x["symbol"] in ["RGTI", "QUBT"]` - Return only entries with symbol RGTI or QUBT.
+    - `expiration_date`
+        - `"expiration_date": lambda x: x["expiration_date"] > (datetime.now() + timedelta(days=30)).date()`  - Return only entries with expiration date more than 30 days from now.
+        - `"expiration_date": lambda x: x["expiration_date"] < (datetime.now() + timedelta(days=30)).date()`  - Return only entries with expiration date less than 30 days from now.
+    - `days_to_expiration`
+        - `"days_to_expiration": lambda x: x["days_to_expiration"] < 45`                - Return only entries with less than 45 days to expiration.
+        - `"days_to_expiration": lambda x: x["days_to_expiration"] > 45`                - Return only entries with more than 45 days to expiration.
+    - `option_type`
+        - `"option_type": lambda x: x["option_type"] == "Call"`                   - Return only entries with option type "Call".
+        - `"option_type": lambda x: x["option_type"] == "Put"`                    - Return only entries with option type "Put".
+    - `volume`
+        - `"volume": lambda x: x["volume"] > 1000`                      - Return only entries with volume greater than 1,000 contracts.
+        - `"volume": lambda x: x["volume"] < 1000`                      - Return only entries with volume less than 1,000 contracts.
+    - `open_interest`
+        - `"open_interest": lambda x: x["open_interest"] > 500`                - Return only entries with open interest greater than 500 contracts.
+        - `"open_interest": lambda x: x["open_interest"] < 500`                - Return only entries with open interest less than 500 contracts.
+    - `volume_open_interest_ratio`
+        - `"volume_open_interest_ratio": lambda x: x["volume_open_interest_ratio"] > 1.5`   - Return only entries with a volume/open interest ratio greater than 1.5.
+        - `"volume_open_interest_ratio": lambda x: x["volume_open_interest_ratio"] < 1.5`   - Return only entries with a volume/open interest ratio less than 1.5.
     """
-    from .utils import session
+    from .utils import get_UOA_url, UOA_Entry, session
+    
+    ua = []
+    url = get_UOA_url(1)
+    response = session.get(url)
+    if response.status_code != 200:
+        response.raise_for_status()
+    print(url)
+    # print(response.json())
+    data: dict = response.json()
+    if not data.get("data"):
+        raise ValueError("No data was found in the response.")
+    ua.extend(data["data"])
+    total = int(data["total"])
+    # print(f"> Total unusual options activity records: {total}")
+    remaining = total - 300
+    if remaining > 0:
+        num_pages = remaining // 300 + 1
+        for page in range(2, num_pages + 2):
+            url = get_UOA_url(page)
+            response = session.get(url)
+            if response.status_code != 200:
+                response.raise_for_status()
+            data = response.json()
+            if not data.get("data"):
+                break
+            ua.extend(data["data"])
+    alldata = dict(
+        data   = ua,
+        total  = total,
+        fields = data["meta"]["field"],
+    )
+    entries = [UOA_Entry(item) for item in alldata["data"]]
+    entries_dicts = [entry.to_dict() for entry in entries]
+    if filters:
+        filtered_activity = [entry for entry in entries_dicts if all(f(entry) for f in filters.values())]
+        entries_dicts = filtered_activity
+    if groupby_symbol:
+        grouped_activity = {}
+        for entry in entries_dicts:
+            symbol = entry["symbol"]
+            if symbol not in grouped_activity:
+                grouped_activity[symbol] = []
+            grouped_activity[symbol].append(entry)
+        entries_dicts = grouped_activity
+        if groupby_optiontype:
+            for symbol, entries in entries_dicts.items():
+                grouped_by_optiontype = {}
+                for entry in entries:
+                    option_type = entry["option_type"]
+                    if option_type not in grouped_by_optiontype:
+                        grouped_by_optiontype[option_type] = []
+                    grouped_by_optiontype[option_type].append(entry)
+                entries_dicts[symbol] = grouped_by_optiontype
+    if groupby_optiontype and not groupby_symbol:
+        grouped_activity = {}
+        for entry in entries_dicts:
+            option_type = entry["option_type"]
+            if option_type not in grouped_activity:
+                grouped_activity[option_type] = []
+            grouped_activity[option_type].append(entry)
+        entries_dicts = grouped_activity
+    if groupby_expiration and not (groupby_symbol or groupby_optiontype):
+        grouped_activity = {}
+        for entry in entries_dicts:
+            expiration_date = entry["expiration_date"]
+            if expiration_date not in grouped_activity:
+                grouped_activity[expiration_date] = []
+            grouped_activity[expiration_date].append(entry)
+        entries_dicts = grouped_activity
+    if groupby_expiration and groupby_optiontype and not groupby_symbol:
+        grouped_activity = {}
+        for entry in entries_dicts:
+            option_type = entry["option_type"]
+            expiration_date = entry["expiration_date"]
+            if option_type not in grouped_activity:
+                grouped_activity[option_type] = {}
+            if expiration_date not in grouped_activity[option_type]:
+                grouped_activity[option_type][expiration_date] = []
+            grouped_activity[option_type][expiration_date].append(entry)
+        # Sort the expiration date keys in each option type by date
+        for option_type, expirations in grouped_activity.items():
+            sorted_expirations = dict(sorted(expirations.items(), key=lambda x: x[0]))
+            grouped_activity[option_type] = sorted_expirations
+        entries_dicts = grouped_activity
+    if groupby_expiration and groupby_symbol and not groupby_optiontype:
+        grouped_activity = {}
+        for entry in entries_dicts:
+            symbol = entry["symbol"]
+            expiration_date = entry["expiration_date"]
+            if symbol not in grouped_activity:
+                grouped_activity[symbol] = {}
+            if expiration_date not in grouped_activity[symbol]:
+                grouped_activity[symbol][expiration_date] = []
+            grouped_activity[symbol][expiration_date].append(entry)
+        # Sort the expiration date keys in each symbol by date
+        for symbol, expirations in grouped_activity.items():
+            sorted_expirations = dict(sorted(expirations.items(), key=lambda x: x[0]))
+            grouped_activity[symbol] = sorted_expirations
+        entries_dicts = grouped_activity
+    if groupby_symbol and groupby_optiontype and groupby_expiration:
+        grouped_activity = {}
+        for symbol, values in entries_dicts.items():
+            for option_type, entries in values.items():
+                for entry in entries:
+                    expiration_date = entry["expiration_date"]
+                    if symbol not in grouped_activity:
+                        grouped_activity[symbol] = {}
+                    if option_type not in grouped_activity[symbol]:
+                        grouped_activity[symbol][option_type] = {}
+                    if expiration_date not in grouped_activity[symbol][option_type]:
+                        grouped_activity[symbol][option_type][expiration_date] = []
+                    grouped_activity[symbol][option_type][expiration_date].append(entry)
+        # Sort the expiration date keys in each symbol and option type by date
+        for symbol, option_types in grouped_activity.items():
+            for option_type, expirations in option_types.items():
+                sorted_expirations = dict(sorted(expirations.items(), key=lambda x: x[0]))
+                grouped_activity[symbol][option_type] = sorted_expirations
+        entries_dicts = grouped_activity
 
-    r = session.get("https://www.barchart.com/proxies/core-api/v1/quotes/get?lists=etfs.us.price_volume.advances.overall&orderDir=desc&fields=symbol%2CsymbolName%2ClastPrice%2CpriceChange%2CpercentChange%2ChighPrice%2ClowPrice%2Cvolume%2CpriceVolume%2CtradeTime%2CsymbolCode%2CsymbolType%2ChasOptions&orderBy=priceVolume&meta=field.shortName%2Cfield.type%2Cfield.description%2Clists.lastUpdate&hasOptions=true&raw=1")
-    r.raise_for_status()
-    data = r.json()
+    return entries_dicts
 
-    print(f"options.test: {str(data)[:20]}")
 
--- barchart/utils.py
+++ barchart/utils.py
@@ -4,9 +4,123 @@
 Utility functions and classes.
 """
 import requests
+import datetime
 import threading
 import urllib.parse
 from typing import Optional
+from dataclasses import dataclass, field, asdict
+
+
+def get_UOA_url(page: int) -> str:
+    """
+    Generates a URL for fetching unusual options activity data from Barchart.
+
+    ### Parameters
+    - `page` ( *int* ) - The page number for pagination.
+
+    ### Returns
+    - `str` - The complete URL for the API request.
+    """
+    # Check if today is Saturday or Sunday
+    def is_weekend() -> bool:
+        return datetime.datetime.now().weekday() in [5, 6]
+    
+    def is_saturday() -> bool:
+        return datetime.datetime.now().weekday() == 5
+    
+    def is_sunday() -> bool:
+        return datetime.datetime.now().weekday() == 6
+
+    if is_weekend():
+        # If today is Saturday, set start_date to yesterday
+        if is_saturday():
+            start_date = datetime.datetime.now() - datetime.timedelta(days=1)
+            end_date = start_date + datetime.timedelta(days=3)
+        # If today is Sunday, set start_date to Friday
+        elif is_sunday():
+            start_date = datetime.datetime.now() - datetime.timedelta(days=2)
+        # Set end_date to Monday
+        end_date = start_date + datetime.timedelta(days=3)
+    else:
+        # If today is a weekday, set start_date to today and end_date to tomorrow
+        start_date = datetime.datetime.now()
+        end_date = start_date + datetime.timedelta(days=1)
+    a = "https://www.barchart.com/proxies/core-api/v1/options/get?fields=symbol%2CbaseSymbol%2CbaseLastPrice%2CbaseSymbolType%2CexpirationDate%2CdaysToExpiration%2CsymbolType%2CstrikePrice%2ClastPrice%2Cmoneyness%2CbidPrice%2CaskPrice%2Cvolume%2CopenInterest%2CvolumeOpenInterestRatio%2CweightedImpliedVolatility%2Cdelta%2CtradeTime%2CsymbolCode&orderBy=volumeOpenInterestRatio&orderDir=desc&baseSymbolTypes=stock&between(volumeOpenInterestRatio%2C1.24%2C)=&between(lastPrice%2C.10%2C)=&between(tradeTime%2C"
+    b = "%2C"
+    c = ")=&between(volume%2C500%2C)=&between(openInterest%2C100%2C)=&in(exchange%2C(AMEX%2CNYSE%2CNASDAQ%2CINDEX-CBOE))=&meta=field.shortName%2Cfield.type%2Cfield.description&page="
+    d = "&limit=300&raw=1"
+    url = a + start_date.strftime("%Y-%m-%d") + b + end_date.strftime("%Y-%m-%d") + c + str(page) + d
+    return url
+
+
+@dataclass
+class UOA_Entry:
+    _raw: dict
+    symbol: str = field(default="")                         # ["baseSymbol"]
+    last_price: float = field(default=0.0)                  # ["raw"]["baseLastPrice"]
+    expiration_date: str = field(default="")                # ["expirationDate"]
+    days_to_expiration: int = field(default=0)              # ["raw"]["daysToExpiration"]
+    option_type: str = field(default="")                    # ["symbolType"]
+    strike_price: float = field(default=0.0)                # ["raw"]["strikePrice"]
+    moneyness: float = field(default="")                    # ["raw"]["moneyness"]
+    bid_price: float = field(default=0.0)                   # ["raw"]["bidPrice"]
+    ask_price: float = field(default=0.0)                   # ["raw"]["askPrice"]
+    volume: int = field(default=0)                          # ["raw"]["volume"]
+    open_interest: int = field(default=0)                   # ["raw"]["openInterest"]
+    volume_open_interest_ratio: float = field(default=0.0)  # ["raw"]["volumeOpenInterestRatio"]
+    implied_volatility: float = field(default=0.0)          # ["raw"]["weightedImpliedVolatility"]
+    delta: float = field(default=0.0)                       # ["raw"]["delta"]
+    last_trade_time: int = field(default="")                # ["raw"]["tradeTime"]
+
+    def __post_init__(self):
+        """
+        Post-initialization processing.
+        """
+        # Validate and set the raw data
+        if not isinstance(self._raw, dict):
+            raise ValueError("Raw data must be a dictionary")
+        if not self._raw:
+            raise ValueError("Raw data cannot be empty")
+        # Set the attributes from the raw data
+        self.symbol = self._raw.get("baseSymbol", "")
+        self.last_price = self._raw.get("raw", {}).get("baseLastPrice", 0.0)
+        self.expiration_date = self._raw.get("expirationDate", "")
+        self.days_to_expiration = self._raw.get("raw", {}).get("daysToExpiration", 0)
+        self.option_type = self._raw.get("symbolType", "")
+        self.strike_price = self._raw.get("raw", {}).get("strikePrice", 0.0)
+        self.moneyness = self._raw.get("raw", {}).get("moneyness", "")
+        self.bid_price = self._raw.get("raw", {}).get("bidPrice", 0.0)
+        self.ask_price = self._raw.get("raw", {}).get("askPrice", 0.0)
+        self.volume = self._raw.get("raw", {}).get("volume", 0)
+        self.open_interest = self._raw.get("raw", {}).get("openInterest", 0)
+        self.volume_open_interest_ratio = self._raw.get("raw", {}).get("volumeOpenInterestRatio", 0.0)
+        self.implied_volatility = self._raw.get("raw", {}).get("weightedImpliedVolatility", 0.0)
+        self.delta = self._raw.get("raw", {}).get("delta", 0.0)
+        self.last_trade_time = self._raw.get("raw", {}).get("tradeTime", "")
+
+        # Convert expiration_date to a datetime object if it's not empty
+        if self.expiration_date:
+            try:
+                # Format: MM/DD/YY
+                self.expiration_date = datetime.datetime.strptime(self.expiration_date, "%m/%d/%y").date()
+            except ValueError:
+                raise ValueError(f"Invalid date format for expiration_date: {self.expiration_date}")
+        # Convert last_trade_time to a datetime object if it's not empty
+        if self.last_trade_time:
+            try:
+                # Format: 1748370476
+                self.last_trade_time = datetime.datetime.fromtimestamp(self.last_trade_time)
+            except ValueError:
+                raise ValueError(f"Invalid date format for last_trade_time: {self.last_trade_time}")
+        
+
+    def to_dict(self) -> dict:
+        """
+        Convert the Entry instance to a dictionary.
+        """
+        d = asdict(self)
+        d.pop('_raw', None)  # Remove the _raw field from the dictionary
+        return d
 
 
 class BarchartSession(requests.Session):
